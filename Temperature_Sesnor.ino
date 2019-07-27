7e45b9b9b73013be651b0f4052d6d34bf510d736
cgeudeker <42182344+cgeudeker@users.noreply.github.com>
2019-07-27 09:49:47 -0700

    Add files via upload

---
+++ Temperature_Sesnor.ino
@@ -0,0 +1,25 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+DallasTemperature sensors(&oneWire);
+void setup() {
+  // put your setup code here, to run once:
+  sensors.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  // Send the command to get temperatures
+  sensors.requestTemperatures(); 
+
+  //print the temperature in Celsius
+  Serial.print("Temperature: ");
+  Serial.print(sensors.getTempCByIndex(0));
+  Serial.print("C  |  ");
+  
+  //print the temperature in Fahrenheit
+  Serial.print((sensors.getTempCByIndex(0) * 9.0) / 5.0 + 32.0);
+  Serial.println("F");
+  delay(2000);
+}
