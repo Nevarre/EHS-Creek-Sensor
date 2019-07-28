95d20e5a0de9465c667a9fca30bda7e918eaa452
Nevarre <jianichen.jan@gmail.com>
2019-07-28 10:13:05 -0700

    Add temperature sensor

---
+++ sensors/Temperature_Sesnor.ino
@@ -0,0 +1,29 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 2
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  // put your setup code here, to run once:
+  sensors.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  // Send the command to get temperatures
+  sensors.requestTemperatures(); 
+
+  //print the temperature in Celsius
+  Serial.print("Temperature: ");
+  Serial.print(sensors.getTempCByIndex(0));
+  Serial.print("C  |  ");
+  
+  //print the temperature in Fahrenheit
+  Serial.print((sensors.getTempCByIndex(0) * 9.0) / 5.0 + 32.0);
+  Serial.println("F");
+  delay(2000);
+}
